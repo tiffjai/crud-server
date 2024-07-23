e8d0d01be41f1aae9e106724ccecce8ce3f5506c
Stephanie <tiffanieho@outlook.com>
2024-07-23 10:18:53 +0100

    added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const fruitRouter = require('./routes/fruitRouter')
 
@@ -6,6 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//CORS
+app.use(cors())
 app.use(express.json())
 app.use('/fruits', fruitRouter)
 
